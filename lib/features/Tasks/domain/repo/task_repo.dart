673f79b1cd38be5451b5003fd9bbf71311bd6e44
Ibrahim Mohammed <hemamohamed.hm35@gmail.com>
2026-02-15 02:32:52 +0200

    fix: Change deleteTask parameter type from String to int

--- lib/features/Tasks/domain/repo/task_repo.dart
+++ lib/features/Tasks/domain/repo/task_repo.dart
@@ -4,7 +4,7 @@ import 'package:tasks_manager/features/Tasks/domain/entities/task_entity.dart';
 
 abstract class TaskRepo {
   Future<Either<Failure, Unit>> addTask(TaskEntity task);
-  Future<Either<Failure, Unit>> deleteTask(String taskId);
+  Future<Either<Failure, Unit>> deleteTask(int taskId);
   Future<Either<Failure, Unit>> updateTask(TaskEntity task);
   Future<Either<Failure, List<TaskEntity>>> getTasks();
 }
