1c1c218c4546bcaafe23785ae70ce384e7c5ad7d
Ibrahim Mohammed <hemamohamed.hm35@gmail.com>
2026-02-17 22:13:54 +0200

    Adds custom search input widget
    Introduces a reusable `CustomSearch` widget to provide a consistent search experience. The widget includes a `TextField` with a search icon, hint text, and a styled border.
    
    Temporarily disables the default counter app widget test as it is no longer relevant to the current development focus.

--- lib/features/Tasks/presenter/ui/widgets/custom_search.dart
+++ lib/features/Tasks/presenter/ui/widgets/custom_search.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+class CustomSearch extends StatefulWidget {
+  final TextEditingController searchController;
+
+  const CustomSearch({super.key, required this.searchController});
+
+  @override
+  State<CustomSearch> createState() => _CustomSearchState();
+}
+
+class _CustomSearchState extends State<CustomSearch> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (value) {},
+      controller: widget.searchController,
+      decoration: InputDecoration(
+        hintText: 'Search tasks...',
+        prefixIcon: const Icon(Icons.search),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide.none,
+        ),
+        filled: true,
+        fillColor: Colors.grey[200],
+      ),
+    );
+  }
+}
