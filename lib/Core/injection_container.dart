00d2b851a3d1baee50b5d079a3a4ee9df07277c6
Ibrahim Mohammed <hemamohamed.hm35@gmail.com>
2026-03-08 01:50:23 +0200

    Sets up task and category feature dependencies
    Registers the task repository and all associated task and category use cases within the dependency injection container. This makes these core business logic components available for consumption by controllers and other parts of the application, enabling full functionality for task and category management.

--- lib/Core/injection_container.dart
+++ lib/Core/injection_container.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:tasks_manager/Core/database/base_locel_data_sources.dart';
 import 'package:tasks_manager/Core/database/database_helper.dart';
+import 'package:tasks_manager/features/Tasks/data/repo/task_repo_imp.dart';
 
 class InjectionContainer extends Bindings {
   @override
@@ -14,10 +15,19 @@ class InjectionContainer extends Bindings {
         databaseHelper: Get.find<DatabaseHelper>(),
       ),
     );
-    // repositories
+    // repositories --> repositories depend on data sources 
+Get.lazyPut<TaskRepoImp>(()=> TaskRepoImp(
+       taskLocalDataSource: Get.find(),
+    ));
 
     // use cases
-
+ Get.lazyPut(() => AddTaskUseCase(Get.find()));
+    Get.lazyPut(() => DeleteTaskUseCase(Get.find()));
+    Get.lazyPut(() => UpdateTaskUseCase(Get.find()));
+    Get.lazyPut(() => GetTasksUseCase(Get.find()));
+    Get.lazyPut(() => GetTasksByCategoryUseCase(Get.find()));
+    Get.lazyPut(() => AddCategoryUseCase(Get.find()));
+    Get.lazyPut(() => GetCategoriesUseCase(Get.find()));
     // controllers
   }
 }
